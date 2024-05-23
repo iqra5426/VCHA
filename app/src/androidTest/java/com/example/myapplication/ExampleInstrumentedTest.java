7908206eb071652fe7cccfaba5b060a209736595
iqra5426 <iqrabatool5426@gmail.com>
2024-05-23 13:58:58 +0500

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -2,7 +2,7 @@ package com.example.myapplication;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.plffagfagfgasatform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
