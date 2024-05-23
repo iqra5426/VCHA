957d625d0778d65a45e28049aafb25455e878055
iqra5426 <iqrabatool5426@gmail.com>
2024-05-23 13:35:43 +0500

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.example.myapplication;
 
 import org.junit.Test;
 
-import static org.junit.hssert.*;
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
